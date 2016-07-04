5bd5979dbb04a596f3acc34d99234ca86c170a8d
catbirdbingo <daniil.bakhtyukov@gmail.com>
2016-07-05 02:31:47 +0300

    add ability to edit profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,5 +6,20 @@ class ProfileController < ApplicationController
   end
 
   def edit
+    @profile = Profile.find(params[:id])
   end
+
+  def update
+    @user = current_user
+    @profile = @user.profile.update(permit_profile)
+    redirect_to profile_path
+  end
+
+
+  private
+
+    def permit_profile
+      params.require(:profile).permit(:first_name, :last_name, :middle_name, :phone, :avatar)
+    end
+
 end
